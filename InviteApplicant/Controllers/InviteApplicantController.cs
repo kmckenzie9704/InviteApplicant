27d68a6eaacd503b97252623a0e7332009460962
Kenneth McKenzieddfsdf <kenneth.mckenzie@myacuity.coms>
2018-03-14 10:25:44 -0400

    Add project files.

---
+++ InviteApplicant/Controllers/InviteApplicantController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InviteApplicant.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+
+namespace InviteApplicant.Controllers
+{
+    [Route("api/InviteApplicant")]
+    public class InviteApplicantController : Controller
+    {
+        private DatabaseContext _context;
+
+        public InviteApplicantController(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET api/InviteApplicant
+        [HttpGet]
+        public string Get()
+        {
+            string strInviteApplicant = "Invite Applicant";
+
+            return strInviteApplicant;
+
+        }
+
+        // POST api/InviteApplicant
+        [HttpPost]
+        public string InviteNewApplicant([FromBody]Applicant applicant)
+        {
+            string strReturn = string.Empty;
+            string strUniqueCode = applicant.appUniqueCode;
+            string strEmail = applicant.appEmail;
+
+            strReturn = Invite(strUniqueCode, strEmail);
+            return strReturn;
+        }
+
+        private string Invite(string strUniqueCode, string strEmail)
+        {
+            string strReturn = string.Empty;
+            using (_context)
+            {
+                Applicant newApplicant = new Applicant
+                {
+                    appEmail = strEmail,
+                    appUniqueCode = strUniqueCode
+                };
+                _context.Applicants.Add(newApplicant);
+                _context.SaveChanges();
+
+                strReturn = "Created";
+                return strReturn;
+            }
+        }
+    }
+
+}
---
+++ InviteApplicant/Models/DatabaseContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace InviteApplicant.Models
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext()
+        { }
+
+        public DatabaseContext(DbContextOptions<DatabaseContext> options)
+        : base(options)
+            { }
+
+        public DbSet<Applicant> Applicants { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            Applicant.OnModelCreating(modelBuilder);
+
+        }
+    }
+}
